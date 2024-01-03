7cb09a6c6a971794d045841449d68ec86497217e
trungnghia12 <daoletrungnghia@gmail.com>
2024-01-03 09:23:02 +0700

    abc

--- lib/nghia/manage_order.dart
+++ lib/nghia/manage_order.dart
@@ -10,6 +10,8 @@ class MyWidget extends StatefulWidget {
 class _MyWidgetState extends State<MyWidget> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: Text('abc'),)
+    );
   }
 }
